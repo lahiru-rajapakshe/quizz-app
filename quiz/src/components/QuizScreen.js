ff6faf7b8b80db49ab91425ee3a6f4a9497a40b7
Lara <lahirurajapakshe.stack@gmail.com>
2022-09-12 22:24:07 +0530

    QuizScreen changes

--- quiz/src/components/QuizScreen.js
+++ quiz/src/components/QuizScreen.js
@@ -1,4 +1,9 @@
+import QuestionList from "../data/questions.json";
+
 function QuizScreen(){
+    const [currentQuestionIndex, setCurrentQuestion]=useState(0);
+    const [markedAnswers,setMarkedAnswers ]=useState(new Array(QuestionList.length))
     return "quiz-screen";
+
 }
 export default QuizScreen;
