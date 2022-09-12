fb6beff448fca0d296cb869741e65b8e63b0c1e3
Lara <lahirurajapakshe.stack@gmail.com>
2022-09-12 22:44:39 +0530

    Create some questionaire paths

--- quiz/src/components/QuizScreen.js
+++ quiz/src/components/QuizScreen.js
@@ -3,7 +3,31 @@ import QuestionList from "../data/questions.json";
 function QuizScreen(){
     const [currentQuestionIndex, setCurrentQuestion]=useState(0);
     const [markedAnswers,setMarkedAnswers ]=useState(new Array(QuestionList.length))
-    return "quiz-screen";
+    const isQuestionEnd = currentQuestionIndex === QuestionList.length;
+
+    return (
+        <div className="quiz-screen">
+            {
+                isQuestionEnd?(
+                    <QuizResult/>
+                ):(
+                    <Question
+                    question={QuestionList[currentQuestionIndex]}
+                    totalQuestions={ QuestionList.length}
+                    currentQuestion={currentQuestionIndex+1}
+                    setAnswer={{index}=>{
+                        setMarkedAnswers((arr)=>
+                        {
+
+                        });
+                    }}
+                    
+                    
+                    />
+                )
+            }
+        </div>
+    )
 
 }
 export default QuizScreen;
